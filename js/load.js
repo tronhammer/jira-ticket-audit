8e7b877bd867bd9cc003844475868f44ea29757b
Sean Murray <smurray@ontraport.com>
2013-09-18 10:03:07 -0700

    gritter not being loaded from base, adding work around for now

--- js/load.js
+++ js/load.js
@@ -18,7 +18,36 @@ var base = "http://jira.ontraport.com//rest/api/2/search",
 		}
 	};
 
-console.log("Processing JQL filter...");
+$("head").append( 
+	$("<link>").attr({
+		rel: "stylesheet", 
+		type: "text/css", 
+		href: "http://boedesign.com/demos/gritter/css/jquery.gritter.css"
+	})
+).append( 
+	$("<script>").attr({
+		src: "http://boedesign.com/demos/gritter/js/jquery.gritter.js", 
+		async: true
+	})
+);
+
+$.gritter.add({
+	// (string | mandatory) the heading of the notification
+	title: 'Processing!',
+	// (string | mandatory) the text inside the notification
+	text: 'Attempting to process current JQL filter for incomplete tickets...',
+	// (string | optional) the image to display on the left
+	// image: 'http://a0.twimg.com/profile_images/59268975/jquery_avatar_bigger.png',
+	// (bool | optional) if you want it to fade out on its own or just sit there
+	// sticky: false,
+	
+    // class_name: 'gritter-light'
+	
+	
+	// (int | optional) the time you want it to be alive for before fading out
+	time: ''
+});
+
 $.get(base + jql).done(function(data){
 	$.each(data.issues, function(){
 		var ticket_name = this.key,
